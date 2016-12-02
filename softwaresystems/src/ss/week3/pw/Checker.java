2a3de1ec682d0369a7fadb9c9bf94bcee0911e87
janwillemtv <janwillemtv1@gmail.com>
2016-12-02 11:42:37 +0100

    opdracht 3.2 en 3.4

--- softwaresystems/src/ss/week3/pw/Checker.java
+++ softwaresystems/src/ss/week3/pw/Checker.java
@@ -3,7 +3,5 @@ package ss.week3.pw;
 /**
  * Created by janwillem on 12/1/16.
  */
-public interface Checker {
-    boolean acceptable(String s);
-    String generatePassword();
+public class Checker {
 }
