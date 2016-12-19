4f0e1e6469d7a8fd6d5f1dd8a2dd1ecaa3b21312
janwillemtv <janwillemtv1@gmail.com>
2016-12-19 16:23:55 +0100

    klaar tot 5.13

--- softwaresystems/src/ss/week5/MapUtil.java
+++ softwaresystems/src/ss/week5/MapUtil.java
@@ -1,34 +1,111 @@
 package ss.week5;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapUtil {
-    public static <K, V> boolean isOneOnOne(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.1
-        return false;
+
+    //@ ensures \result == isOneOnOne(map);
+    //@ ensures (\result || !\result);
+    //@ requires map != null;
+    //@ ensures \result ==(\forall K x, y; map.keyset().contains(x)
+    //@ && map.keyset().contains(y) && x != y; map.get(x) !=map.get(y)
+    public /*@pure*/ static <K, V> boolean isOneOnOne(Map<K, V> map) {
+
+        for (V value : map.values()) {
+            boolean found = false;
+
+            for (K key : map.keySet()) {
+                if (map.get(key) == value) {
+                    if (found) {
+                        return false;
+                    }
+                    else {
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        return true;
     }
-    public static <K, V> 
-           boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-5.2
-        return false;
+
+    //@ ensures (\result || !\result);
+    //@ requires range != null;
+    //@ requires map != null;
+    public /*@pure*/ static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
+        for (V rangeValue : range) {
+            if (!map.values().contains(rangeValue)) {
+                return false;
+            }
+
+        }
+
+        return true;
+
     }
+
+    //@ requires map != null;
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+        Map<V, Set<K>> newMap = new HashMap<>();
+
+        for(Map.Entry<K, V> entry : map.entrySet()){
+            K key = entry.getKey();
+            V value = entry.getValue();
+
+            if (!newMap.containsKey(value)) {
+                newMap.put(value, new HashSet<K>(Collections.singletonList(key)));
+            }
+            else {
+                Set<K> duplicate = newMap.get(value);
+                duplicate.add(key);
+
+                newMap.put(value, duplicate);
+            }
+        }
+
+        return newMap;
 	}
+
+    //@ requires map != null;
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
-        return null;
+        Map<V, K> newMap = new HashMap<>();
+
+        if (isOneOnOne(map) && isSurjectiveOnRange(map, new HashSet<>(map.values()))) {
+            for (Map.Entry<K, V> entry : map.entrySet()) {
+                newMap.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return newMap;
 	}
-	public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.4
-        return false;
+
+    //@ ensures (\result || !\result);
+    //@ requires f != null;
+    //@ requires g 1= null;
+	public /*@pure*/ static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
+
+        for (Map.Entry<K, V> value : f.entrySet()) {
+            if(!g.containsKey(value.getValue())){
+                return false;
+            }
+        }
+        return true;
 	}
+
+    //@ ensures \typeof(\result) == \type(Map<K,W>) || \result == null;
+    //@ requires f != null;
+    //@ requires g != null;
 	public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-        // TODO: implement, see exercise P-5.5
+        Map<K, W> newMap = new HashMap<>();
+
+        if (compatible(f, g)) {
+            for (Map.Entry<K, V> entry : f.entrySet()) {
+                newMap.put(entry.getKey(), g.get(entry.getValue()));
+            }
+
+            return newMap;
+        }
+
         return null;
-	}
+    }
+
 }
