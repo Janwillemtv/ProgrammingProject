eb5d0b9d9aae71e2d481206cd2b8c60d65df036e
janwillemtv <janwillemtv1@gmail.com>
2016-12-20 11:15:37 +0100

    week 6 added

--- softwaresystems/src/ss/week5/EncodingTest.java
+++ softwaresystems/src/ss/week5/EncodingTest.java
@@ -13,20 +13,28 @@ import org.apache.commons.codec.binary.Hex;
 public class EncodingTest {
     public static void main(String[] args) throws DecoderException {
         String input = "Hello World";
+        String hexRandom = "4d6f64756c652032";
         String hexString = "010203040506";
         String random = "U29mdHdhcmUgU31zdGVtcw==";
-        String encoded = Hex.encodeHexString(input.getBytes());
-        System.out.println(encoded);
-        String randomOut = new String(Base64.decodeBase64(random));
-        System.out.println(randomOut);
-       // System.out.println(Base64.)
-        System.out.println(Base64.encodeBase64String(input.getBytes()));
-        byte[] output = Hex.decodeHex(encoded.toCharArray());
-        byte[] uncoded = Hex.decodeHex(hexString.toCharArray());
-        System.out.println(Base64.encodeBase64(uncoded));
 
+        String encoded = Hex.encodeHexString(input.getBytes());
+        System.out.println("5.11 encode hex: " + encoded);
+
+        byte[] output = Hex.decodeHex(hexRandom.toCharArray());
         String outputDecoded = new String(output);
-        System.out.println(outputDecoded);
+        System.out.println("5.12 decode hex: " + outputDecoded);
+
+        String randomOut = new String(Base64.decodeBase64(random));
+        System.out.println("5.13 - base64 decode: " + randomOut);
+
+        System.out.println("5.13 - base64 encode: " + Base64.encodeBase64String(input.getBytes()));
+
+        byte[] uncoded = Hex.decodeHex(hexString.toCharArray());
+        System.out.println("5.13 - decode hex encode base64: " + Base64.encodeBase64(uncoded));
+
+
+
+        System.out.println("5.13 encode base64:");
         String out = "a";
         for (int i = 0; i< 10; i++) {
             String hereOut = new String(Base64.encodeBase64String(out.getBytes()));
