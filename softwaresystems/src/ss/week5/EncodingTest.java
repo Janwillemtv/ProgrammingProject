711428077f164f6dffc9dc91effb09b4d1704062
TomOnderwater <t.onderwater@hotmail.com>
2016-12-20 10:30:47 +0100

    Naive player is geadd

--- softwaresystems/src/ss/week5/EncodingTest.java
+++ softwaresystems/src/ss/week5/EncodingTest.java
@@ -1,4 +1,4 @@
-package ss.week5;
+/*package ss.week5;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
@@ -8,10 +8,11 @@ import org.apache.commons.codec.binary.Hex;
  * of the Apache Commons Codec library.
  *
  */
+/*
 public class EncodingTest {
     public static void main(String[] args) throws DecoderException {
         String input = "Hello World";
 
         System.out.println(Hex.encodeHexString(input.getBytes()));
     }
-}
+}*/
