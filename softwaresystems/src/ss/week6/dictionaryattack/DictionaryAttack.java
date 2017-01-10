b3031a691f05e8ce3eff98adb5624468ecf1f7fe
TomOnderwater <t.onderwater@hotmail.com>
2017-01-10 10:44:48 +0100

    Passwords zijn gebroken

--- softwaresystems/src/ss/week6/dictionaryattack/DictionaryAttack.java
+++ softwaresystems/src/ss/week6/dictionaryattack/DictionaryAttack.java
@@ -1,6 +1,14 @@
 package ss.week6.dictionaryattack;
 
+import org.apache.commons.codec.binary.Hex;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.security.MessageDigest;
+
 
 
 public class DictionaryAttack {
@@ -16,8 +24,30 @@ public class DictionaryAttack {
 	 * the username, and the password hash should be the content.
 	 * @param filename
 	 */
-	public void readPasswords(String filename) {
-		// To implement        
+	public DictionaryAttack() {
+		passwordMap = new HashMap<>();
+		hashDictionary = new HashMap<>();
+	}
+	public void readPasswords(String filename) throws IOException {
+        System.out.println("Test2");
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+                String[] split = line.split(":\\s+");
+
+                if (split.length == 2) {
+                    String username = split[0];
+                    System.out.println(username);
+                    passwordMap.put(username, split[1]);
+                }
+            }
+        }
+        catch (IOException ex) {
+            throw ex;
+        }
 	}
 
 	/**
@@ -28,7 +58,19 @@ public class DictionaryAttack {
 	 */
 	public String getPasswordHash(String password) {
     		// To implement
-    		return null;
+			String hash = null;
+
+			try {
+				MessageDigest digester = MessageDigest.getInstance("MD5");
+				byte[] all = digester.digest(password.getBytes());
+
+				hash = Hex.encodeHexString(all);
+				//System.out.println(hash);
+
+			}  catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+			}
+		return hash;
 	}
 	/**
 	 * Checks the password for the user the password list. If the user
@@ -37,9 +79,14 @@ public class DictionaryAttack {
 	 * @param password
 	 * @return whether the password for that user was correct.
 	 */
+
 	public boolean checkPassword(String user, String password) {
         // To implement
-		return false;
+		if (!passwordMap.containsKey(user)) {
+			return false;
+		}
+		String hash = getPasswordHash(password);
+		return (passwordMap.get(user).equals(hash));
 	}
 
 	/**
@@ -48,18 +95,55 @@ public class DictionaryAttack {
      * the original password.
 	 * @param filename filename of the dictionary.
 	 */
-    	public void addToHashDictionary(String filename) {
-        // To implement        
-    }
+    	public void addToHashDictionary(String filename) throws IOException {
+            // To implement
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(filename));
+
+                String word;
+                while ((word = in.readLine()) != null) {
+                    String hash = getPasswordHash(word);
+                   // System.out.println(word);
+                    hashDictionary.put(hash, word);
+                }
+            } catch (IOException e) {
+                throw e;
+            }
+        }
 	/**
 	 * Do the dictionary attack.
 	 */
 	public void doDictionaryAttack() {
 		// To implement
+        int i =0;
+       // System.out.println("test");
+		for (Map.Entry<String, String>entry : passwordMap.entrySet()) {
+			String user = entry.getKey();
+			//System.out.println("here now");
+			String passwordHash = entry.getValue();
+			System.out.println("");
+			i++;
+
+			if (hashDictionary.containsKey(passwordHash)) {
+				System.out.println("User: " + user + " Password: " + hashDictionary.get(passwordHash));
+			}
+		}
+		System.out.println(i);
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		String PATH = "C:/softwaresystems/ProgrammingProject/softwaresystems/src/ss/week6/dictionaryattack/";
+
+
 		DictionaryAttack da = new DictionaryAttack();
 		// To implement
+		try {
+		da.addToHashDictionary(PATH + "Dictionary.txt");
+		da.addToHashDictionary(PATH + "Passwords.txt");
+		//System.out.println("Test this");
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+		da.readPasswords(PATH + "Passwords.txt");
 		da.doDictionaryAttack();
 	}
 
