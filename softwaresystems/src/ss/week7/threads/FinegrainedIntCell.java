6dcaf6079a184562430ad2144e8b42e5145b60d9
TomOnderwater <t.onderwater@hotmail.com>
2017-01-16 15:46:49 +0100

    producer consumer

---
+++ softwaresystems/src/ss/week7/threads/FinegrainedIntCell.java
@@ -0,0 +1,64 @@
+package ss.week7.threads;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Tom on 16-1-2017.
+ */
+public class FinegrainedIntCell implements IntCell {
+    private Lock l;
+    private Condition canWrite;
+    private Condition canRead;
+    private int value;
+    private boolean write;
+
+    public FinegrainedIntCell(){
+     l =  new ReentrantLock();
+        canWrite =  l.newCondition();
+        canRead = l.newCondition();
+        value = 0;
+        write = false;
+    }
+
+    @Override
+    public void setValue(int valueArg) {
+        l.lock();
+
+
+            try {
+                while (write) {
+                    canWrite.await();
+                }
+                //System.out.println(value);
+                this.value = valueArg;
+                this.write = true;
+                canRead.signal();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                l.unlock();
+            }
+
+    }
+    @Override
+    public int getValue() {
+        l.lock();
+
+        try {
+            while (!write) {
+                canRead.await();
+
+            }
+            this.write = false;
+            canWrite.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            l.unlock();
+        }
+        return value;
+    }
+        // System.out.println(value);
+}
---
+++ softwaresystems/src/ss/week7/threads/SynchronizedIntCell.java
@@ -0,0 +1,43 @@
+package ss.week7.threads;
+
+/**
+ * Created by Tom on 16-1-2017.
+ */
+public class SynchronizedIntCell implements IntCell {
+    private int value = 0;
+
+    private boolean write = false;
+
+    public synchronized void setValue(int valueArg) {
+
+        this.value = valueArg;
+        while (write) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(value);
+        this.value = valueArg;
+        this.write = true;
+        notifyAll();
+    }
+
+    public synchronized int getValue() {
+
+        while (!write) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+       // System.out.println(value);
+        notifyAll();
+        this.write = false;
+        return value;
+    }
+
+
+}
