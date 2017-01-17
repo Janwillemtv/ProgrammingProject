9bee0ca5eb75473e746a81966a059a59ea2b4f36
TomOnderwater <t.onderwater@hotmail.com>
2017-01-17 13:45:21 +0100

    Deel 1 van de server is af

--- softwaresystems/src/ss/week7/cmdline/Peer.java
+++ softwaresystems/src/ss/week7/cmdline/Peer.java
@@ -1,11 +1,8 @@
 package ss.week7.cmdline;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Peer for a simple client-server application
@@ -19,7 +16,7 @@ public class Peer implements Runnable {
     protected Socket sock;
     protected BufferedReader in;
     protected BufferedWriter out;
-
+    protected Scanner Terminal;
 
     /*@
        requires (nameArg != null) && (sockArg != null);
@@ -31,6 +28,17 @@ public class Peer implements Runnable {
      */
     public Peer(String nameArg, Socket sockArg) throws IOException
     {
+        this.name = nameArg;
+        this.sock = sockArg;
+
+        InputStream instream = sock.getInputStream();
+        OutputStream outstream = sock.getOutputStream();
+
+        this.in = new BufferedReader(new InputStreamReader(instream));
+        this.out = new BufferedWriter(new OutputStreamWriter(outstream));
+
+        Terminal = new Scanner(System.in);
+
     }
 
     /**
@@ -38,6 +46,24 @@ public class Peer implements Runnable {
      * writes the characters to the default output.
      */
     public void run() {
+        String input = "";
+        while(true) {
+            try {
+                if (in.ready()) {
+                    try {
+                        input = in.readLine();
+                        System.out.println(input);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (input.equals("EXIT")) {
+                break;
+            }
+        }
     }
 
 
@@ -46,13 +72,47 @@ public class Peer implements Runnable {
      * the socket-connection to the Peer process.
      * On Peer.EXIT the method ends
      */
-    public void handleTerminalInput() {
+    public void handleTerminalInput() throws IOException {
+        String lineIn = "";
+        while(true) {
+            if (Terminal.hasNextLine()) {
+                lineIn = Terminal.nextLine();
+                System.out.println(lineIn);
+            }
+
+
+            try {
+                out.write(lineIn, 0, lineIn.length());
+                out.newLine();
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (lineIn.equals("EXIT")) {
+                break;
+            }
+        }
     }
 
     /**
      * Closes the connection, the sockets will be terminated
      */
     public void shutDown() {
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            sock.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**  returns name of the peer object*/
--- softwaresystems/src/ss/week7/cmdline/Server.java
+++ softwaresystems/src/ss/week7/cmdline/Server.java
@@ -18,6 +18,55 @@ public class Server {
 
     /** Starts a Server-application. */
     public static void main(String[] args) {
+        if (args.length !=2) {
+            System.out.println(USAGE);
+            System.exit(0);
+        }
+
+        String name = args[0];
+        InetAddress addr = null;
+        int port = 0;
+        ServerSocket sock = null;
+        Socket s;
+
+
+        // parse args[2] - the port
+        try {
+            port = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println(USAGE);
+            System.out.println("ERROR: port " + args[2]
+                    + " is not an integer");
+            System.exit(0);
+        }
+
+        // try to open a Socket to the server
+        try {
+            sock = new ServerSocket(port);
+
+            System.out.println("Server socket created. Waiting for clients");
+
+            s = sock.accept();
+
+            System.out.println("Connection: " + s.getInetAddress().getHostName() + " : " + s.getPort());
+
+            Peer peer = new Peer(name,s);
+
+            Thread t = new Thread(peer);
+            t.start();
+            peer.handleTerminalInput();
+            peer.shutDown();
+            t.interrupt();
+
+            sock.close();
+
+        } catch (IOException e) {
+            System.out.println("ERROR: could not create a socket on " + addr
+                    + " and port " + port);
+        }
+
+
     }
+
 
 } // end of class Server
